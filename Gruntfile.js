3e3b348b7b49d0bf914ef44951974a6a827e74b8
José Carneiro <josemcarneiro@hotmail.com>
2015-02-13 19:13:42 +0000

    Made index into under construction page

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,12 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+
     less: {
       development: {
         options: {
-          paths: ["css"]
+          paths: ["css"],
+          cleancss: true
         },
         files: {"css/style.css": "less/style.less"}
       },
@@ -15,9 +17,59 @@ module.exports = function(grunt) {
         },
         files: {"css/style.css": "less/style.less"}
       }
+    },
+
+    jade: {
+      compile: {
+        options: {
+          pretty: true,
+          data: {
+            debug: false
+          }
+        },
+        files: {"index.html": "jade/*.jade"}
+      }
+    },
+
+    watch: {
+      jade: {
+        files: ["./jade/*"],
+        tasks: ["jade"],
+        options: {
+          livereload: true
+        },
+      },
+      less: {
+        files: ["less/*"],
+        tasks: ["less:development"],
+        options: {
+          livereload: true
+        },
+      },
+      configFiles: {
+        files: [ 'Gruntfile.js' ],
+        options: {
+          reload: true
+        }
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          open: true,
+          hostname: "localhost",
+          livereload: true
+        }
+      }
     }
+
   });
 
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.registerTask('default', ['less']);
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.registerTask('default', ['connect:server', 'watch']);
+
 };
