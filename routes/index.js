18752ee97690a68e0255b09745888758455b84c2
José Carneiro <josemcarneiro@hotmail.com>
2016-08-05 20:56:12 +0100

    Commit. I don't know what has changed :P

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,18 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
   res.locals.meta.description = 'José Maria Carneiro. Business Student, Web Developer.';
+  res.locals.data = {
+    social: [{
+      name: 'jose@light.pt',
+      url: 'mailto:jose@light.pt'
+    }, {
+      name: 'facebook@josemcarneiro',
+      url: '//facebook.com/josemcarneiro'
+    }, {
+      name: 'instagram@josemcarneiro',
+      url: '//instagram.com/josemcarneiro'
+    }]
+  }
   res.render('index');
 });
 
