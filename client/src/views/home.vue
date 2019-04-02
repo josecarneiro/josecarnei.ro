54d5e46343427a01f730533ca78dc93f7e89e6ab
Jose Carneiro <contact@josecarnei.ro>
2019-04-02 01:43:44 +0100

    Updated descriptions, social links, descriptions

--- client/src/views/home.vue
+++ client/src/views/home.vue
@@ -2,7 +2,13 @@
   section.full-height
     .container
       h1 José Carneiro
-      h2 Full-Stack Developer
+      h2
+        | Founder of 
+        a(href="https://iindie.com/?ref=josecarneiro") iindie.co
+        //- br
+        |  / Full-Stack Developer
+        //- br
+        |  / Independent Consultant
       //- h5 Lisbon, Portugal
       ul
         li(v-for="link of social")
@@ -15,25 +21,31 @@
 <script>
   export default {
     data: () => ({
-      social: [{
-        name: 'contact@josecarnei.ro',
-        url: 'mailto:contact@josecarnei.ro'
-      }, {
-        name: 'github@josecarneiro',
-        url: 'https://github.com/josecarneiro'
-      }, {
-        name: 'linkedin@josemcarneiro',
-        url: 'https://linkedin.com/in/josemcarneiro'
-      }, {
-        name: 'instagram@josecarnei.ro',
-        url: 'https://instagram.com/josecarnei.ro'
-      }, {
-        name: 'facebook@josemcarneiro',
-        url: 'https://facebook.com/josemcarneiro'
-      }, {
-        name: 'dribbble@josec',
-        url: 'https://dribbble.com/josec'
-      }]
+      social: [
+        {
+          name: 'contact@josecarnei.ro',
+          url: 'mailto:contact@josecarnei.ro'
+        }, {
+          name: 'twitter@josemcarneiro',
+          url: 'https://twitter.com/josemcarneiro'
+        }, {
+          name: 'github@josecarneiro',
+          url: 'https://github.com/josecarneiro'
+        }, {
+          name: 'linkedin@josemcarneiro',
+          url: 'https://linkedin.com/in/josemcarneiro'
+        }, {
+          name: 'dribbble@josec',
+          url: 'https://dribbble.com/josec'
+        }, {
+          name: 'instagram@josecarnei.ro',
+          url: 'https://instagram.com/josecarnei.ro'
+        }
+        // {
+        //   name: 'facebook@josemcarneiro',
+        //   url: 'https://facebook.com/josemcarneiro'
+        // }
+      ]
     })
   };
 </script>
