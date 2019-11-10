71202c4ee5fd5d1031ed2b31183d1d4977c89e37
José Carneiro <josemcarneiro@gmail.com>
2019-11-10 14:53:12 +0000

    Updated personal information, changed font family to monospace

--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,11 @@
 import Vue from "vue";
 import App from "./app";
 import router from "./router";
-// import store from "./store";
 import "./register-service-worker";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  // store,
   render: h => h(App)
 }).$mount("#shell");
--- client/src/views/home.vue
+++ client/src/views/home.vue
@@ -3,10 +3,11 @@
     .container
       h1 José Carneiro
       h2
-        | Founder of 
-        a(href="https://iindie.com/?ref=josecarneiro") iindie.co
+        | Lead Instructor at Ironhack Lisboa
+        //- |  / Entrepreneur
         |  / Full-Stack Developer
-        |  / Independent Consultant
+        //- |  / Independent Consultant
+        |  / Entrepreneur
       ul
         li(v-for="link of social")
           a(
@@ -16,33 +17,38 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      social: [
-        {
-          name: 'contact@josecarnei.ro',
-          url: 'mailto:contact@josecarnei.ro'
-        }, {
-          name: 'twitter@josemcarneiro',
-          url: 'https://twitter.com/josemcarneiro'
-        }, {
-          name: 'github@josecarneiro',
-          url: 'https://github.com/josecarneiro'
-        }, {
-          name: 'linkedin@josemcarneiro',
-          url: 'https://linkedin.com/in/josemcarneiro'
-        }, {
-          name: 'dribbble@josec',
-          url: 'https://dribbble.com/josec'
-        }, {
-          name: 'instagram@josecarnei.ro',
-          url: 'https://instagram.com/josecarnei.ro'
-        }
-        // {
-        //   name: 'facebook@josemcarneiro',
-        //   url: 'https://facebook.com/josemcarneiro'
-        // }
-      ]
-    })
-  };
+export default {
+  data: () => ({
+    social: [
+      {
+        name: "contact@josecarnei.ro",
+        url: "mailto:contact@josecarnei.ro"
+      },
+      {
+        name: "github@josecarneiro",
+        url: "https://github.com/josecarneiro"
+      },
+      {
+        name: "twitter@josemcarneiro",
+        url: "https://twitter.com/josemcarneiro"
+      },
+      {
+        name: "linkedin@josemcarneiro",
+        url: "https://linkedin.com/in/josemcarneiro"
+      },
+      {
+        name: "instagram@josecarnei.ro",
+        url: "https://instagram.com/josecarnei.ro"
+      }
+      // {
+      //   name: 'dribbble@josec',
+      //   url: 'https://dribbble.com/josec'
+      // }
+      // {
+      //   name: 'facebook@josemcarneiro',
+      //   url: 'https://facebook.com/josemcarneiro'
+      // }
+    ]
+  })
+};
 </script>
