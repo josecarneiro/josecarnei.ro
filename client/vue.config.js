54c66d05a22b1133b84faeeaf6bf9d96b7585b4d
José Carneiro <josemcarneiro@gmail.com>
2019-12-12 18:06:42 +0000

    Added netlify configuration, fixes to service worker

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,11 +1,11 @@
 const metaData = {
-  title: "José Maria Carneiro",
-  description: "José Maria Carneiro. Full-Stack Developer.",
+  title: 'José Maria Carneiro',
+  description: 'José Maria Carneiro. Full-Stack Developer.',
   analyticsKey: process.env.GOOGLE_ANALYTICS_KEY,
   pwa: {
-    name: "josecarnei.ro",
-    themeColor: "#f3f3f3",
-    msTileColor: "#ffffff"
+    name: 'josecarnei.ro',
+    themeColor: '#f3f3f3',
+    msTileColor: '#ffffff'
   }
 };
 
@@ -13,12 +13,17 @@ module.exports = {
   outputDir: './../dist',
   lintOnSave: false,
   chainWebpack: config => {
-    config.plugin("html").tap(args => {
+    config.plugin('html').tap(args => {
       Object.assign(args[0], { data: { meta: metaData } });
       return args;
     });
   },
-  pwa: metaData.pwa,
+  pwa: {
+    ...metaData.pwa,
+    workboxOptions: {
+      skipWaiting: true
+    }
+  },
   pluginOptions: {
     // critical: {
     //   width: 1920,
