cf520834fad00d463e1744931caffd57427efe2a
José Carneiro <josemcarneiro@gmail.com>
2019-12-13 10:39:21 +0000

    Removed unused styles

--- client/src/app.vue
+++ client/src/app.vue
@@ -1,4 +1,4 @@
-<template lang="pug">
+<template lang="pug" functional>
   .shell
     router-view
 </template>
--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,11 @@
-import Vue from "vue";
-import App from "./app";
-import router from "./router";
-import "./register-service-worker";
+import Vue from 'vue';
+import App from './app';
+import router from './router';
+import './register-service-worker';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#shell");
+}).$mount('#shell');
