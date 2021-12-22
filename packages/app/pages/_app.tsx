68535688b74df9301ac837d64f10cf7ebced7c92
José Carneiro <jose.carneiro@mercedes-benz.io>
2021-12-22 19:27:56 +0000

    Add Google font import to to document

--- packages/app/pages/_app.tsx
+++ packages/app/pages/_app.tsx
@@ -13,7 +13,6 @@ const App: FunctionComponent<AppProps> = ({ Component, pageProps }) => (
     <Head>
       <title>José Carneiro</title>
       <meta name="description" content="José Carneiro" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="robots" content="index,follow,noodp" />
       <meta name="googlebot" content="index,follow" />
       <link rel="icon" href="/favicon.ico" />
@@ -21,11 +20,6 @@ const App: FunctionComponent<AppProps> = ({ Component, pageProps }) => (
         name="theme-color"
         content="<%= htmlWebpackPlugin.options.data.meta.pwa.themeColor %>"
       />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        rel="stylesheet"
-        href={`https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&display=${FONT_BEHAVIOR}`}
-      ></link>
     </Head>
     <ThemeProvider>
       <PageWrapper>
